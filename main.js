ab7fd09f614a15231a7ce630051c07697f494fe9
MikulkovaZ <mikulkova.zuzana@seznam.cz>
2021-03-14 01:20:40 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+let ctverecek = document.querySelector(".ctverecek");
+
+
+function pocitej() {
+  ctverecek.style.backgroundColor = 'green';
+  document.querySelector("#vysledek").innerHTML = secti(4, 5);
+  alert("Gratulace, právě jsi spustila tutu funkci!");
+  console.log("Gratulace, právě jsi spustila tuut funkci!");
+  document.querySelector(".ctverecek").innerHTML = "Gratulace, právě jsi spustila tutu funkci!";
+  ctverecek.style.lineHeight = "25px";
+}
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
